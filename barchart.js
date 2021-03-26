fdb7cbd5fe36b35b29b6972458b1ca67c788f6a8
Matt Macdonald <mksmacdonald@gmail.com>
2021-03-26 22:22:52 +0000

    shaping into functional. Most wanted features stripped to meet exacting definition of requirements

--- barchart.js
+++ barchart.js
@@ -1,7 +1,20 @@
 
 
+const numberToLetters = [, 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+
+const drawBarChart = function (data, options, element) {
+    
+}
+
 function buttonPush( ) {  
-    alert("You pushed the graph button");
+    $("#graphOne").removeClass("percentage percentage-1");
+    $("#graphOne").addClass("percentage percentage-90");
     //$("percentage").after("<dd class="percentage percentage-16"><span class="text">Firefox: 16.09%</span></dd>")
+
+    for(let i = 1; i <= 10; i++){
+        let id = '#graph' + numberToLetters[i];
+        //alert(id);
+       // $(id).removeClass("percentage percentage-1");
+    }
 }
 
