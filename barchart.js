bdfb7db649effb93849859c06dc6debf9dc18dd9
Matt Macdonald <mksmacdonald@gmail.com>
2021-03-26 17:06:54 +0000

    syncing with windows

--- barchart.js
+++ barchart.js
@@ -1,3 +1,7 @@
+
+
 function buttonPush( ) {  
-    alert("You pushed the graph button");  
+    alert("You pushed the graph button");
+    //$("percentage").after("<dd class="percentage percentage-16"><span class="text">Firefox: 16.09%</span></dd>")
 }
+
