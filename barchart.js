183813b3d22b98f3ade07e83646e641a07fa8280
Matt Macdonald <mksmacdonald@gmail.com>
2021-03-28 19:47:57 +0000

    Started creating a vertical Bar chart

--- barchart.js
+++ barchart.js
@@ -1,33 +1,54 @@
 
 
-const numberToLetters = [, 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+const numberToLetters = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven'];
 
-const dataToGraph = [88, 33, 53, 7, 93, 5, 15, 1, 18, 66];
+//const dataToGraph = [88, 33, 53, 7, 93, 5, 15, 1, 18, 66];
+const dataToGraph = [88, 33, 53, 7, 93, 5, 15, 1, 18, 66, 8, 9 , 10, 11, 12, 13, 14];
 
-const drawBarChart = function (data, options, element) {
+const drawVerticalBarChart = function (data, show, options, element) {
+    console.log('draw vertical bar chart ran');
+    
+    for(let i = 1; i <= 20; i++) {
+        let id = '#vG' + i;
+        $(id).hide();
+        console.log('we hid ' + id);
+    }
 
-    console.log('draw bar chart ran');
-
-
-    for(let i = 1; i <= 10 && data[i]; i++){
-
-        console.log('this ran');
-
-        let id = '#graph' + numberToLetters[i];
-        let newClass = "percentage percentage-" + data[i];
-        //alert(id);
-        $(id).removeClass("percentage percentage-1");
-        $(id).addClass(newClass);
-        $(id + " > span.text").append(data[i - 1]);
+    for(let i = 1; i <= data.length; i++) {
+        let id = '#vG' + i;
+        $(id).show();
+        $(id).css("width", 50 / data.length + "%");
+        console.log('we unhid ' + id);
     }
 }
 
-function buttonPush( ) {  
-  //  $("#graphOne").removeClass("percentage percentage-1");
- //   $("#graphOne").addClass("percentage percentage-90");
+const drawHorizontalBarChart = function (data, show, options, element) {
+
+    console.log('draw horizontal bar chart ran');
+
+    for (let i = 1; i <= data.length && data[i - 1]; i++) {
+        let id = '#graph' + numberToLetters[i - 1];
+        let newClass = "percentage percentage-" + data[i];
+
+        //console.log(i + ' ' + id);
+
+        $(id).removeClass("percentage percentage-1");
+        $(id).addClass(newClass);
+        if (!show) {
+            $(id).hide();
+        }
+
+        $(id + " > span.text").append(data[i - 1]);
+        $(id + " > span.text").css("left", 200);
+    }
+}
+
+function buttonPush() {
+    //  $("#graphOne").removeClass("percentage percentage-1");
+    //   $("#graphOne").addClass("percentage percentage-90");
     //$("percentage").after("<dd class="percentage percentage-16"><span class="text">Firefox: 16.09%</span></dd>")
 
-    drawBarChart(dataToGraph);
+
 
 
 }
